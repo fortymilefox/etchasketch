615dc9e05ba4e26f8d57d1a37f0dadc510438ae7
Keith Gordon <a.keithgordon27@gmail.com>
2021-04-22 17:17:11 -0400

    customize slider

--- scripts.js
+++ scripts.js
@@ -12,16 +12,33 @@ makeGrid = () => {
   }
 };
 
+function randomColor() {
+  let randomRed = Math.floor(Math.random() * 256);
+  let randomGreen = Math.floor(Math.random() * 256);
+  let randomBlue = Math.floor(Math.random() * 256);
+  return `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+}
+
+function removeAllChildNodes(parent){
+  while(parent.firstChild){
+    parent.removeChild(parent.firstChild);
+  }
+};
+
 const slider = document.querySelector('#slider')
 const gridValue = document.querySelector('.value');
 slider.addEventListener('input', function(){
   let newValue = document.getElementById('slider').value;
   gridValue.textContent = newValue;
   removeAllChildNodes(gridContainer);
-  gridContainer.setAttribute('style', `grid-template-colums: repeat(${newValue}, 2fr); grid-template-rows: repeat(${newValue}, 2fr);`)
+  gridContainer.setAttribute('style', `grid-template-columns: repeat(${newValue}, 2fr); grid-template-rows: repeat(${newValue}, 2fr);`)
   for (let i = 0; i < newValue * newValue; i++) {
     const div = document.createElement('div');
     div.classList.add('cell');
+    div.addEventListener('mouseover', function(event){
+      event.target.style.backgroundColor = 'black';
+    })
+    gridContainer.appendChild(div);
   }
 });
 
@@ -33,5 +50,28 @@ clearGrid.addEventListener('click', function(){
     cell[i].style.backgroundColor = 'rgba(248, 248, 210, 0.952)';
   }
 });
+
+const rainbowPen = document.querySelector('#rainbow');
+rainbowPen.addEventListener('click', function(){
+  let newValue = document.getElementById('slider').value;
+  let cell = gridContainer.children;
+  for (let i=0; i < newValue*newValue; i++) {
+    cell[i].addEventListener('mouseover', function(event){
+      event.target.style.backgroundColor = randomColor();
+    })
+  }
+});
+
+const defaultPen = document.querySelector('#default');
+defaultPen.addEventListener('click', function(){
+  let newValue = document.getElementById('slider').value;
+  let cell = gridContainer.children;
+  for (let i=0; i < newValue*newValue; i++) {
+    cell[i].addEventListener('mouseover', function(event){
+      event.target.style.backgroundColor = 'black';
+    })
+  }
+});
+
 
 makeGrid();
